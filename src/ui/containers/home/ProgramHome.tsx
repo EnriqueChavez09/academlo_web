a96e3ec8adb45e4bddc733b298c302c35a479c7e
Enrique <rchavezs@uni.pe>
2023-05-05 17:37:52 -0500

    feat: maquetanto la seccion de program del home

--- src/ui/containers/home/ProgramHome.tsx
+++ src/ui/containers/home/ProgramHome.tsx
@@ -1,3 +1,43 @@
+import { ButtonCommon } from '@/ui/components/common'
+
 export const ProgramHome = () => {
-  return <div>ProgramHome</div>
+  const redirectApplication = () => {
+    window.location.href = '/application'
+  }
+  return (
+    <div className="programHome">
+      <div className="programHome__text">
+        <p>Seamless service targeting your unique needs</p>
+        <div></div>
+        <p>
+          Lifestyle choices vary, which is why we take the time to learn about
+          your individual situation and requirements.
+        </p>
+        <p>
+          We’ll assist you from start to ﬁnish, ensuring you have the insight
+          and knowledge needed to succeed.
+        </p>
+      </div>
+      <div className="programHome__residency">
+        <div className="programHome__residency__circle"></div>
+        <div className="programHome__residency__text">
+          <p>Residency Programs</p>
+          <div></div>
+          <p>
+            Mexican residency safeguards against future instability and opens up
+            a world of opportunities for investors and their families. A Golden
+            Visa permit provides visa-free access to many countries across the
+            globe.
+          </p>
+        </div>
+        <div className="programHome__residency__button">
+          <ButtonCommon
+            customClassName="buttonHome"
+            text="Get started"
+            buttonRunFunction={redirectApplication}
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
